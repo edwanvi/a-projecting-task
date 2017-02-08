558e412e790f5d347dfcf23166809da91d25a027
Edwan Vi <tkdberger@gmail.com>
2017-02-08 13:30:29 -0500

    Add more constants

--- constants.py
+++ constants.py
@@ -4,7 +4,9 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREY = (211, 211, 211)
 RED = (255, 0, 0)
-LIGHTBLUE = (75, 0, 130)
+LIGHTBLUE = (75, 0, 130) # hey this isn't light at all
+ENEMY_MOVE_SPEED = 1.25
+GRAVITY_FACTOR = 0.35
 
 # Screen dimensions
 SCREEN_WIDTH = 800
