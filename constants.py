55c6c2a17ad619fec21c6dec66cef7771878ac38
Edwan Vi <tkdberger@gmail.com>
2017-02-10 15:13:27 -0500

    Increase move speed

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ BLUE = (0, 0, 255)
 GREY = (211, 211, 211)
 RED = (255, 0, 0)
 LIGHTBLUE = (75, 0, 130) # hey this isn't light at all
-ENEMY_MOVE_SPEED = 1.25
+ENEMY_MOVE_SPEED = 6
 GRAVITY_FACTOR = 0.35
 
 # Screen dimensions
